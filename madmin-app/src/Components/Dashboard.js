736ea31882a085ddffc5deea3deb2721fddce6f5
nuvallo <nuvallo.inc@gmail.com>
2020-02-23 22:20:48 -0500

    Vistor Component

--- madmin-app/src/Components/Dashboard.js
+++ madmin-app/src/Components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Stats } from "./Stats";
+import { Vistors } from "./Vistors";
 
 export const Dashboard = () => {
   return (
     <div>
       <Stats />
+      <Vistors />
     </div>
   );
 };
---
+++ madmin-app/src/Components/Vistors.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+export const Vistors = () => {
+  return (
+    <section className="section section-visitors blue lighten-4">
+      <div className="row">
+        <div className="col s12 m6 l8">
+          <div className="card-panel">
+            <div id="chartContainer"></div>
+          </div>
+        </div>
+        <div className="col s12 m6 l4">
+          <ul className="collection with-header latest-comments">
+            <li className="collection-header">
+              <h5>Latest Comments</h5>
+            </li>
+            <li className="collection-item avatar">
+              <img src="img/person1.jpg" alt="" className="circle" />
+              <span className="title">John Doe</span>
+              <p className="truncate">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt minima dolor error laboriosam autem ad beatae explicabo
+                pariatur maxime fuga sed quod quo voluptas, adipisci illum
+                aspernatur est, fugit tempore.
+              </p>
+              <a href="" className="approve green-text">
+                Approve
+              </a>
+              |
+              <a href="" className="deny red-text">
+                Deny
+              </a>
+            </li>
+            <li className="collection-item avatar">
+              <img src="img/person2.jpg" alt="" class="circle" />
+              <span className="title">Steve Smith</span>
+              <p className="truncate">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt minima dolor error laboriosam autem ad beatae explicabo
+                pariatur maxime fuga sed quod quo voluptas, adipisci illum
+                aspernatur est, fugit tempore.
+              </p>
+              <a href="" class="approve green-text">
+                Approve
+              </a>
+              |
+              <a href="" class="deny red-text">
+                Deny
+              </a>
+            </li>
+            <li className="collection-item avatar">
+              <img src="img/person3.jpg" alt="" class="circle" />
+              <span className="title">Ellen Williams</span>
+              <p className="truncate">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt minima dolor error laboriosam autem ad beatae explicabo
+                pariatur maxime fuga sed quod quo voluptas, adipisci illum
+                aspernatur est, fugit tempore.
+              </p>
+              <a href="" class="approve green-text">
+                Approve
+              </a>
+              |
+              <a href="" class="deny red-text">
+                Deny
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
