b368dfc6a9f07dc50ab7603ce51e0be2597965c7
nuvallo <nuvallo.inc@gmail.com>
2020-02-23 23:05:40 -0500

    Login auth works

--- madmin-app/src/Components/Login.js
+++ madmin-app/src/Components/Login.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { LoginNavigation } from "./Navigation/LoginNavigation";
 
-export const Login = () => {
+export const Login = props => {
+  const [cred, setCred] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleChange = event => {
+    console.log(event.target.value);
+    setCred({ ...cred, [event.target.name]: event.target.value });
+  };
+
+  const submitLogin = event => {
+    event.preventDefault();
+    axiosWithAuth()
+      .post("/login", cred)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/protected");
+      })
+      .catch(err => {
+        localStorage.removeItem("token");
+        console.log("invalid login ", err);
+      });
+  };
+
   return (
     <>
       <LoginNavigation />
@@ -14,14 +39,27 @@ export const Login = () => {
                 <form action="index.html">
                   <div className="input-field">
                     <i className="material-icons prefix">email</i>
-                    <input type="email" id="email" />
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={cred.email}
+                      onChange={handleChange}
+                    />
                     <label className="white-text" htmlFor="email">
                       Email Address
                     </label>
                   </div>
                   <div className="input-field">
                     <i className="material-icons prefix">lock</i>
-                    <input type="password" className="password" id="password" />
+                    <input
+                      type="password"
+                      className="password"
+                      id="password"
+                      name="password"
+                      value={cred.password}
+                      onChange={handleChange}
+                    />
                     <label className="white-text" htmlFor="password">
                       password
                     </label>
@@ -30,6 +68,7 @@ export const Login = () => {
                     type="submit"
                     value="login"
                     className="btn btn-large btn-extended grey lighten-4 black-text"
+                    onClick={submitLogin}
                   />
                 </form>
               </div>
