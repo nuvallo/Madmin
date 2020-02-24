97b9e6b4403a883d8446d58db985bd2df850350a
nuvallo <nuvallo.inc@gmail.com>
2020-02-23 22:07:32 -0500

    Login Component Created

--- madmin-app/src/App.js
+++ madmin-app/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Login } from "./Components/Login";
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
--- madmin-app/src/Components/Login.js
+++ madmin-app/src/Components/Login.js
@@ -1,5 +1,38 @@
 import React from "react";
 
 export const Login = () => {
-  return <div></div>;
+  return (
+    <section className="section section-login">
+      <div className="container">
+        <div className="row">
+          <div className="col s12 m8 offset-m2 l6 offset-l3">
+            <div className="card-panel login blue darken-2 white-text center">
+              <h2>Madmin Login</h2>
+              <form action="index.html">
+                <div className="input-field">
+                  <i className="material-icons prefix">email</i>
+                  <input type="email" id="email" />
+                  <label className="white-text" for="email">
+                    Email Address
+                  </label>
+                </div>
+                <div className="input-field">
+                  <i className="material-icons prefix">lock</i>
+                  <input className="password" id="password" />
+                  <label className="white-text" for="password">
+                    password
+                  </label>
+                </div>
+                <input
+                  type="submit"
+                  value="login"
+                  className="btn btn-large btn-extended grey lighten-4 black-text"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
