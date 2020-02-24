6f3bc123608327da849c6b1d120576fa8d469bf9
nuvallo <nuvallo.inc@gmail.com>
2020-02-23 22:47:39 -0500

    authWithAxios created

--- madmin-app/src/App.js
+++ madmin-app/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { Route } from "react-browser-router";
+import { Route, Switch } from "react-browser-router";
 import { Dashboard } from "./Components/Dashboard";
 import { Login } from "./Components/Login";
+import ProtectedRoute from "./Components/ProtectedRoute";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Route extact path="/" component={Login} />
+      <Switch>
+        <ProtectedRoute exact path="/protected" component={Dashboard} />
+        <Route exact path="/" component={Login} />
+      </Switch>
     </div>
   );
 }
